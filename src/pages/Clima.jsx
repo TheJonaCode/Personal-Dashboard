4fdd87957ddaa3e87fa5252ce9af5a962b3b054d
TheJonaCode <mcjhona069@gmail.com>
2025-06-30 22:25:56 -0600

    Example Card Clima

--- src/pages/Clima.jsx
+++ src/pages/Clima.jsx
@@ -10,7 +10,7 @@ function Clima() {
     if (ciudad.trim() === "") return;
 
     try {
-      const apiKey = "9e72d9cc00bcf04e6363fac694e4f2f3"; 
+      const apiKey = import.meta.env.VITE_API_KEY;
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric&lang=es`;
 
       const respuesta = await axios.get(url);
@@ -30,6 +30,20 @@ function Clima() {
         <input type="text" className="form-control" placeholder="Escribe la ciudad" value={ciudad} onChange={(e) => setCiudad(e.target.value)} />
         <button onClick={obtenerClima} className="btn btn-primary"> Buscar </button>
       </div>
+
+      <div className="card text-bg-light mt-3">
+      <div className="card-body">
+        <h3 className="card-title">Ciudad Ejemplo</h3>
+        <p className="card-text">
+          <b>Temperatura:</b> 22°C <br />
+          <b>Clima:</b> Soleado
+        </p>
+        <img
+          src="https://openweathermap.org/img/wn/01d@2x.png"
+          alt="Ícono clima"
+        />
+      </div>
+    </div>
 
       {error && <div className="alert alert-danger">{error}</div>}
 
