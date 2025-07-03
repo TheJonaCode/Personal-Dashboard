30d36a7d08bfe29a1471660a8b7cd56f4900ee6e
TheJonaCode <mcjhona069@gmail.com>
2025-07-02 21:59:38 -0600

    obteniendo api clima y mostrando connsultas

--- src/pages/Clima.jsx
+++ src/pages/Clima.jsx
@@ -10,7 +10,8 @@ function Clima() {
     if (ciudad.trim() === "") return;
 
     try {
-      const apiKey = import.meta.env.VITE_API_KEY;
+      const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
+
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric&lang=es`;
 
       const respuesta = await axios.get(url);
@@ -24,36 +25,22 @@ function Clima() {
   };
 
   return (
-    <div className="container mt-4">
+      <div className="container mt-4">
       <h2>Consulta el Clima</h2>
       <div className="input-group mt-3 mb-3">
         <input type="text" className="form-control" placeholder="Escribe la ciudad" value={ciudad} onChange={(e) => setCiudad(e.target.value)} />
         <button onClick={obtenerClima} className="btn btn-primary"> Buscar </button>
       </div>
 
-      <div className="card text-bg-light mt-3">
-      <div className="card-body">
-        <h3 className="card-title">Ciudad Ejemplo</h3>
-        <p className="card-text">
-          <b>Temperatura:</b> 22°C <br />
-          <b>Clima:</b> Soleado
-        </p>
-        <img
-          src="https://openweathermap.org/img/wn/01d@2x.png"
-          alt="Ícono clima"
-        />
-      </div>
-    </div>
-
       {error && <div className="alert alert-danger">{error}</div>}
 
       {datosClima && (
-        <div className="card mt-3">
+        <div className="card text-bg-light mt-3">
           <div className="card-body">
             <h3 className="card-title">{datosClima.name}</h3>
             <p className="card-text">
-              Temperatura: {datosClima.main.temp}°C <br />
-              Clima: {datosClima.weather[0].description} <br />
+              <b>Temperatura:</b> {datosClima.main.temp}°C <br />
+              <b>Clima:</b> {datosClima.weather[0].description} <br />
             </p>
             <img
               src={`https://openweathermap.org/img/wn/${datosClima.weather[0].icon}@2x.png`}
